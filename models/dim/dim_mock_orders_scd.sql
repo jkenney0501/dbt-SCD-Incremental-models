768041d47b652bc4d2cca98395880224d770563a
James Kenney <james.kenney@astricadata.com>
2024-07-12 20:47:56 +0000

    update dim mock orders scd, nothing material, just  code aesthetic

--- models/dim/dim_mock_orders_scd.sql
+++ models/dim/dim_mock_orders_scd.sql
@@ -12,15 +12,15 @@ FROM {{ ref('scd_mock_orders') }}
 ),
 --  fills the null of dbt valid to with a date far in the future, this happens AFTER the is current flag is defined so it does not over write it.
 dim_transform_valid_to AS(
-    SELECT *
+    SELECT 
+        order_id,
+        status,
+        valid_from,
+        CASE WHEN valid_to IS NULL THEN '2099-01-01' ELSE valid_to END AS valid_to,
+        updated_at,
+        is_current
     FROM dim_transformed_scd
 )
 
-SELECT 
-order_id,
-status,
-valid_from,
-CASE WHEN valid_to IS NULL THEN '2099-01-01' ELSE valid_to END AS valid_to,
-updated_at,
-is_current
+SELECT *
 FROM dim_transform_valid_to
